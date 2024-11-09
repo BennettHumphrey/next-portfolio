6e639ec2b7153c15a66d0bc276705282b1c2346c
Ben Humphrey <bennetthumphrey98@gmail.com>
2024-11-09 17:52:44 -0500

    Add nav motion

--- src/app/Components/Nav/Nav.tsx
+++ src/app/Components/Nav/Nav.tsx
@@ -6,6 +6,8 @@ import { BiMenuAltRight } from 'react-icons/bi';
 import useOutsideAlerter from '@/app/hooks/useOutsideAlerter';
 import useHeaderShadow from '@/app/hooks/useHeaderShadow';
 import { useWindowSize } from 'react-use';
+import { AnimatePresence, motion } from 'framer-motion';
+import { menu } from 'framer-motion/client';
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -28,23 +30,27 @@ const Nav = () => {
     >
       <div className="flex justify-between items-center max-w-screen-lg mx-auto">
         <p className="text-2xl tracking-wide`">Bennett Humphrey</p>
-        <ul
-          ref={menuRef}
-          className={`flex gap-6 text-sm uppercase transition-transform duration-300 ${
-            menuOpen ? 'flex-col absolute right-0 top-16 p-8 bg-bg-light rounded-lg shadow-lg w-2/3 md:w-1/3' : 'hidden md:flex'
-          }`}
-        >
-          <li>
-            <a href="#expertise" className="hover:text-blue-500">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" className="hover:text-blue-500">
-              Projects
-            </a>
-          </li>
-        </ul>
+        <AnimatePresence>
+          {menuOpen && <motion.ul
+            initial={{ x: 300, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 300, opacity: 0 }}
+            transition={{ type: "spring", duration: 0.5, stiffness: 100 }}
+            ref={menuRef}
+            className='flex gap-6 text-sm uppercase flex-col absolute right-0 top-16 p-8 bg-bg-light rounded-lg shadow-lg w-2/3 md:w-1/3'
+          >
+            <li>
+              <a href="#expertise" className="hover:text-blue-500">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="hover:text-blue-500">
+                Projects
+              </a>
+            </li>
+          </motion.ul>}
+        </AnimatePresence>
         <div
           className="md:hidden"
           onClick={() => setMenuOpen(!menuOpen)}
